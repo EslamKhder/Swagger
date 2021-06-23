aef7fed722f6aeb2a85fb2b08de0814707bb8f33
EslamKhder <eslamkhder81@gmail.com>
2021-06-23 18:31:30 +0200

    ApiInfo

--- src/main/java/com/spring/swagger/confige/SwaggerConfig.java
+++ src/main/java/com/spring/swagger/confige/SwaggerConfig.java
@@ -4,9 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
+
+@EnableSwagger2
 @Configuration
 public class SwaggerConfig {
 
@@ -14,9 +20,24 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                // .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.any())
                 .apis(RequestHandlerSelectors.basePackage("com.spring.swagger"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(metaData());
+    }
+
+    private ApiInfo metaData(){
+        Contact contact = new Contact("Eslam Khder","https://www.facebook.com/eslam.khder.98/","eslamkhder11@gmail.com");
+        return new ApiInfo (
+                "Spring Framework with Swagger",
+                "Employee & Player Controller",
+                "3.0.0",
+                "https://swagger.io/",
+                contact,
+                "Licence https://swagger.io/",
+                "https://swagger.io",
+                new ArrayList<>()
+                );
     }
 }
