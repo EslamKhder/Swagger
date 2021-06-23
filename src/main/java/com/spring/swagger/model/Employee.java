484e9e5d26767a3604db4a56d6187d32ad0d0218
EslamKhder <eslamkhder81@gmail.com>
2021-06-23 17:27:26 +0200

    @ApiModelProperty

--- src/main/java/com/spring/swagger/model/Employee.java
+++ src/main/java/com/spring/swagger/model/Employee.java
@@ -13,8 +13,10 @@ public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "employee_id")
+    @ApiModelProperty(value = "this is id of employee")
     private Long id;
 
+    @ApiModelProperty(value = "this is Full Name of employee",required = false)
     private String fullName;
 
     private String age;
