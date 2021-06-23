af4f401416dca167d60d9ee09e16c4da343ef109
EslamKhder <eslamkhder81@gmail.com>
2021-06-23 14:36:11 +0200

    @Api - @Tag

--- src/main/java/com/spring/swagger/controller/EmployeeController.java
+++ src/main/java/com/spring/swagger/controller/EmployeeController.java
@@ -2,10 +2,14 @@ package com.spring.swagger.controller;
 
 import com.spring.swagger.dao.EmployeeRepository;
 import com.spring.swagger.model.Employee;
+import io.swagger.annotations.Api;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 // http://localhost:8080
+@Api(value = "this is employee controller",description = "Controller Layer")
+@Tag(name = "Employee Controller Layer")
 @RestController
 @RequestMapping("/api")
 // http://localhost:8080/api
@@ -16,18 +20,22 @@ public class EmployeeController {
 
     // http://localhost:8080/api/save
     @PostMapping("/save")
+    @Tag(name = "Employee Controller Layer")
     public void save(@RequestBody Employee employee){
         employeeRepository.save(employee);
     }
     @GetMapping("/show")
+    @Tag(name = "Emplyee Controller Layer")
     public Employee show(@RequestParam int id){
         return employeeRepository.findById((long) id).get();
     }
     @DeleteMapping("/delete")
+    @Tag(name = "Emplyee Controller Layer")
     public void delete(@RequestParam int id){
         employeeRepository.deleteById((long) id);
     }
     @PutMapping("/edit")
+    @Tag(name = "Emplyee Controller Layer")
     public void edit(@RequestBody Employee employee){
         employeeRepository.save(employee);
     }
